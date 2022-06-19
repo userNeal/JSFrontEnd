a3cbd272441f11c44413aa6e863821c795064767
userNeal <dk1060@scarletmail.rutgers.edu>
2022-06-19 22:08:31 +0900

    sdadad

--- js/todo.js
+++ js/todo.js
@@ -1,54 +1,53 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
-const toDoList = document.querySelector("#todo-list");
+const toDoList = document.getElementById("todo-list");
 
-let todos = []; 
-// reason for let instead of const.
-// not to replace as you add lists after refreshing.
 const TODOS_KEY = "todos";
+let toDos = [];
 
 function saveToDos() {
-    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
-    // JSON.stringify basically works 
-    // like you convert the variable to the string type.
-
-    // Whenever you push an element to the todo array,
-    // that means you will put string one by one
-    // Otherwise, you will save as like "the string of array."
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function handleClickButton(event) {
-    const li = event.target.parentElement;
-    li.remove(); 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
-function paintToDo(newToDo) { 
+function paintToDo(newTodo) {
     const li = document.createElement("li");
-    li.id = newToDo.id;
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newToDo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
-    button.addEventListener("click", handleClickButton);
+    button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
 }
 
+
 function handleToDoSubmit(event) {
-    event.preventDefault();
-    const newTodo = toDoInput.value;
-    toDoInput.value = "";
-    todos.push(newTodo);
-    paintToDo(newTodo);
-    saveToDos();
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(), // time for now in seconds
+  };
+   toDos.push(newTodoObj);
+   paintToDo(newTodoObj);
+   saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
-
 if (savedToDos !== null) {
-  const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo); // after refreshing, this works.
 }
